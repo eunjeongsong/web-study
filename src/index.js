f37a8f0f0b3e6565fdfa47a3c8f4e6287b3ec061
eunjeongsong <hellogine12@gmail.com>
2018-08-10 00:04:55 +0900

    create webpack sample project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div><h1>Hello</h1><img src="./images/icon.png" /></div>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV == 'development' ? 'development' : 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,      // regx
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'main.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({  // Also generate a test.html
+            filename: 'index.html',
+            template: 'public/index.html'
+        })  
+    ],
+    devServer:{
+        contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'assets')]
+    }
+}
